047cef1364fac996b4119f93718c91f8b71027bf
normaberrones1 <normaberrones.tx@gmail.com>
2024-12-05 20:10:44 -0500

    Models

--- Savor-Spice/src/main/java/com/savorspice/model/UserRecipes.java
+++ Savor-Spice/src/main/java/com/savorspice/model/UserRecipes.java
@@ -1,4 +1,24 @@
 package com.savorspice.model;
 
 public class UserRecipes {
+
+    private int user_id;
+
+    private int recipe_id;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getRecipe_id() {
+        return recipe_id;
+    }
+
+    public void setRecipe_id(int recipe_id) {
+        this.recipe_id = recipe_id;
+    }
 }
